fe7ac7a03e7728e4f542b70745a6ac3a9e7fe210
Vachilla64 <68883197+Vachilla64@users.noreply.github.com>
2024-06-03 20:28:33 +0100

    created Succes.jsx Page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
+import Succes from './Pages/Succes';
 
 function App() {
   return (
     <div className="App">
+      <Succes />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
