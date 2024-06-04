400024d5841c684b1499cbb2b5269c2b2cfe0d03
Vachilla64 <68883197+Vachilla64@users.noreply.github.com>
2024-06-04 21:25:08 +0100

    added 2nd eye svg source for closed eye asset

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -3,6 +3,7 @@ import google_svg from "./Google.svg";
 import facebook_svg from "./Facebook.svg";
 import apple_svg from "./Apple.svg";
 import eye_svg from "./Eye.svg"
+import eyeCl_svg from "./Eye.svg"
 import warning_svg from "./Warning.svg"
 import linedont_svg from "./linedont.svg"
 import back_svg from "./back.svg"
@@ -13,6 +14,7 @@ export {
     facebook_svg,
     apple_svg,
     eye_svg,
+    eyeCl_svg,
     warning_svg,
     linedont_svg,
     back_svg,
