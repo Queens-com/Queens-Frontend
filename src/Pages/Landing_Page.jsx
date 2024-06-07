b99a2ba3e2bd9becd254ff353ec154b6082462a5
Vachilla64 <68883197+Vachilla64@users.noreply.github.com>
2024-06-07 19:00:23 +0100

    added base design for navbar, weill need alot of revision as isnt full responsibe yet

---
+++ src/Pages/Landing_Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
+
+const Landing_Page = () => {
+  return (
+    <div className='Landing_Page'>
+        <Navbar />
+        <Footer />
+    </div>
+  )
+}
+
+export default Landing_Page
