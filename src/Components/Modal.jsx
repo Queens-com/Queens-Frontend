7f9ed725049d80756895d4ebb4a8b46a76f69c91
Vachilla64 <68883197+Vachilla64@users.noreply.github.com>
2024-07-01 14:44:37 +0100

    slight modification to modal ui

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Modal = ({ open, onClose, children }) => {
-  const handleClick = (e) => {
-    e.stopPropagation();
-    onClose();
-  };
-
   return (
-    <div onClick={onClose} className={`${open ? 'visible' : 'invisible'} fixed inset-0 flex items-center justify-center bg-black/30`}>
-      <div onClick={(e) => e.stopPropagation()} className={`${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'} w-[80vw] max-w-[35rem] bg-white rounded-xl shadow p-6 transition`}>
-        {children || <p>No content</p>} 
+    <div onClick={onClose} className={`fixed inset-0 flex items-center justify-center bg-black/30 transition ${open ? 'visible opacity-100' : 'invisible opacity-0'}`}>
+      <div onClick={(e) => e.stopPropagation()} className={`w-[80vw] max-w-[35rem] bg-white rounded-xl shadow p-6 transform transition-all ${open ? 'scale-100 opacity-100' : 'scale-75 opacity-0'}`}>
+        {children || <p>No content</p>}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
